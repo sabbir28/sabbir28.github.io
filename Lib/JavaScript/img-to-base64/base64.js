42414dedc00d4b78187d49242fc6cb0ee853174c
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2023-09-25 07:55:45 +0000

    new file:   Lib/JavaScript/img-to-base64/base64.js 	deleted:    Lib/JavaScript/img-to-base64/browser.js 	deleted:    Lib/JavaScript/img-to-base64/browser.min.js 	deleted:    Lib/JavaScript/img-to-base64/image-to-base64.js 	deleted:    Lib/JavaScript/img-to-base64/image-to-base64.min.js 	deleted:    Lib/JavaScript/img-to-base64/test/index.js 	deleted:    Lib/JavaScript/img-to-base64/test/nodejs.png

---
+++ Lib/JavaScript/img-to-base64/base64.js
@@ -0,0 +1,31 @@
+// Create a JavaScript library for image to base64 conversion
+var ImageToBase64 = {
+    fromURL: function (imageUrl, callback) {
+      var img = new Image();
+      img.crossOrigin = 'Anonymous';
+  
+      img.onload = function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+  
+        var base64Data = canvas.toDataURL('image/png');
+        callback(base64Data);
+      };
+  
+      img.src = imageUrl;
+    },
+  
+    fromFile: function (file, callback) {
+      var reader = new FileReader();
+  
+      reader.onload = function (e) {
+        var base64Data = e.target.result;
+        callback(base64Data);
+      };
+  
+      reader.readAsDataURL(file);
+    }
+};
