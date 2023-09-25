6000e127171a67fc1f495785a8cfc79f107bb494
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2023-09-25 08:06:23 +0000

    new file:   Lib/JavaScript/hex/HexLib.js

---
+++ Lib/JavaScript/hex/HexLib.js
@@ -0,0 +1,28 @@
+function base64ToHex(base64Data) {
+    // Remove the data URI prefix and split it into the data type and base64 portion
+    const parts = base64Data.split(';base64,');
+    if (parts.length !== 2) {
+      throw new Error('Invalid base64 data URI format');
+    }
+  
+    // Decode the base64 data and convert it to a hexadecimal representation
+    const binaryData = atob(parts[1]);
+    const byteArray = new Uint8Array(binaryData.length);
+    for (let i = 0; i < binaryData.length; i++) {
+      byteArray[i] = binaryData.charCodeAt(i);
+    }
+  
+    const hexArray = Array.from(byteArray).map((byte) => byte.toString(16).padStart(2, '0'));
+    return hexArray.join('');
+}
+
+function hexToBase64(hexData, mimeType) {
+    // Convert the hexadecimal representation to binary data
+    const hexArray = hexData.match(/.{1,2}/g);
+    const byteArray = new Uint8Array(hexArray.map((byte) => parseInt(byte, 16)));
+  
+    // Convert the binary data to a base64 encoded string
+    const binaryData = String.fromCharCode.apply(null, byteArray);
+    const base64Data = `data:${mimeType};base64,` + btoa(binaryData);
+    return base64Data;
+}
