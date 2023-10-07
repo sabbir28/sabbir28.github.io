ec5a97ac453a3bae5261358ca3e5de7d5cf62bcc
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2023-10-07 16:48:42 +0600

    Update gui_script.sh

--- s/gui_script.sh
+++ s/gui_script.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-# Use Zenity to prompt the user to select a directory to start the scan
-selected_directory=$(zenity --file-selection --directory --title="Select a Directory to Scan")
+# Use Zenity to create a password entry dialog for the username
+username=$(zenity --entry --title="Login" --text="Enter Username:" --entry-text "admin")
 
-# Check if the user canceled the directory selection
+# Check if the user canceled the username entry
 if [ $? -ne 0 ]; then
-    zenity --info --text="Scan canceled."
+    zenity --info --text="Login canceled."
     exit 1
 fi
 
-# Use find to scan all files and directories starting from the selected directory
-scan_result=$(find "$selected_directory")
+# Use Zenity to create a password entry dialog for the password
+password=$(zenity --password --title="Login" --text="Enter Password:")
 
-# Use Zenity to display the scan result in a text box
-zenity --text-info --title="File Scanner" --width=800 --height=600 --text="$scan_result"
+# Check if the user canceled the password entry
+if [ $? -ne 0 ]; then
+    zenity --info --text="Login canceled."
+    exit 1
+fi
+
+# Check if the username and password are not empty
+if [ -z "$username" ] || [ -z "$password" ]; then
+    zenity --error --text="Username and password must not be empty."
+    # exit 1
+fi
+
+# Check if the username is "admin" and the password is "root"
+if [ "$username" == "admin" ] && [ "$password" == "root" ]; then
+    zenity --info --text="Login successful."
+    xdg-open "https://sabbir28.github.io/"
+else
+    zenity --error --text="Login failed. Invalid username or password."
+fi
